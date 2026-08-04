ae94b30a03c1c1c7fda9b2c2e5fabe691df38e15
lucasnassuato2025-cloud <lucasnassuato2025@gmail.com>
2026-08-04 19:28:36 -0300

    Exportar novos módulos do CRM

---
+++ crm/app/operations/index.ts
@@ -0,0 +1,4 @@
+export { TasksModule } from "./tasks-module";
+export { InstallmentsPanel } from "./installments-panel";
+export { ReportsModule } from "./reports-module";
+export { recordActivity } from "./shared";
